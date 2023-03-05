75136cf1c294371f8e54dcea388b027a0e7e6f61
Vlad <71086815+swixge@users.noreply.github.com>
2023-03-05 20:16:11 +0200

    exem

--- src/main.adb
+++ src/main.adb
@@ -1,6 +1,36 @@
+with Ada.Text_IO;
+
 procedure Main is
 
+   can_stop : boolean := false;
+   pragma Atomic(can_stop);
+
+   task type break_thread;
+   task type main_thread;
+
+   task body break_thread is
+   begin
+      delay 15.0;
+      can_stop := true;
+   end break_thread;
+
+   task body main_thread is
+      sum : Long_Long_Integer := 0;
+   begin
+      loop
+         sum := sum + 1;
+         exit when can_stop;
+      end loop;
+      delay 1.0;
+
+      Ada.Text_IO.Put_Line(sum'Img);
+   end main_thread;
+
+   b1 : break_thread;
+   t1 : main_thread;
+   t2 : main_thread;
+   t3 : main_thread;
+   t4 : main_thread;
 begin
-   --  Insert code here.
    null;
 end Main;
